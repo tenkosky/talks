b2e66bb4791ebc963d85eb94f4680f0077055204
Tom Enkosky <tomenk@bu.edu>
2016-09-18 10:04:51 -0400

    first commit

---
+++ Graph Flow Talk/slides/GeoGebra/HTML5/5.0/web3d/deferredjs/46A50BF51D154390A2D12AE24C2D1FB5/5.cache.js
@@ -0,0 +1,1 @@
+$wnd.web3d.runAsyncCallback5('O1b(1,null,{});_.gC=function Nb(){var a;N7l[a=++O7l]=Nb;O7l=a-1;return this.cZ};M7l(dl)(5);\n//# sourceURL=web3d-5.js\n')
---
+++ Graph Flow Talk/slopes-first-vis/GeoGebra/HTML5/5.0/web/deferredjs/C498B97FB5CB8F209DBD836C4DFD0F4F/5.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback5('mXb(1,null,{});_.gC=function Nb(){var a;gQk[a=++hQk]=Nb;hQk=a-1;return this.cZ};fQk(cl)(5);\n//# sourceURL=web-5.js\n')
